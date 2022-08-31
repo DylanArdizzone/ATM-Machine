001c6aa17adc368d05b6188ce8b6bc2c0abd6009
DylanArdizzone <dardizzone1@hwemail.com>
2022-08-30 18:23:08 -0700

    all methods created not tested
    tester still empty

--- src/ATM.java
+++ src/ATM.java
@@ -7,4 +7,50 @@ public class ATM {
 		accounts = new HashMap<Integer, Double>();
 	}
 	
+	public void openAccount(Integer accNum) {
+		accounts.put(accNum, 0.0);
+	}
+	
+	public void openAccount(Integer accNum, double deposit) {
+		accounts.put(accNum, deposit);
+	}
+	
+	public void closeAccount(Integer accNum) {
+		accounts.remove(accNum);
+	}
+	
+	public double checkBalance(Integer accNum) {
+		if(accounts.get(accNum) == null) {
+			return 0.0;
+		}
+		return accounts.get(accNum);
+	}
+	
+	public boolean depositMoney(Integer accNum, double deposit) {
+		if(accounts.get(accNum) == null) {
+			return false;
+		}
+		if(deposit <0) {
+			return false;
+		}
+		double k = accounts.get(accNum) + deposit;
+		accounts.put(accNum, k);
+		return true;
+	}
+	
+	public boolean withdrawMoney(Integer accNum, double withdraw) {
+		if(accounts.get(accNum) == null) {
+			return false;
+		}
+		if(withdraw <0) {
+			return false;
+		}
+		if(withdraw > accounts.get(accNum)) {
+			return false;
+		}
+		double k = accounts.get(accNum) - withdraw;
+		accounts.put(accNum, k);
+		return true;
+	}
+	
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,8 +1,13 @@
 
 public class Tester {
-
 	public static void main(String[] args) {
-		
+		ATM k = new ATM();
+		k.openAccount(5);
+		k.openAccount(6, 109.84);
+		k.closeAccount(5);
+		k.closeAccount(6);
+		System.out.println(k.checkBalance(5));
+		System.out.println(k.checkBalance(6));
 
 	}
 
